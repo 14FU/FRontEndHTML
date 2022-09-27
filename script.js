73a5f59a4ff4bd6c2d519bc9c3df14998dc331d7
14FU <fauzia53.u@gmail.com>
2022-09-27 11:02:51 +0100

    java task one completed

--- script.js
+++ script.js
@@ -10,3 +10,4 @@ console.log("And your star sign");
 let make= "Mercedes Benz"
 let model= "C class"
 console.log("my favourite car is ",make,"and the model is",model);
+console.log("This is now %c the end of the exercises","color:crimson;font-family: fantasy;font-style:bold;background color:black;padding: 10px");
