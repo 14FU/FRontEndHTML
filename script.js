4f47c9102f68381b0bf5f79589d374e41dcc1591
14FU <fauzia53.u@gmail.com>
2022-09-27 11:22:30 +0100

    task two varibles

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ let make= "Mercedes Benz"
 let model= "C class"
 console.log("my favourite car is ",make,"and the model is",model);
 console.log("This is now %c the end of the exercises","color:crimson;font-family: fantasy;font-style:bold;background color:black;padding: 10px");
+
+// task two
+let a;
+let b="12345";
+let c= 12345
+let d= true;
+let e= {a:"javascriptyy"}
