2ce6b7e1eb17b2f6673d89c891deeb2af7775c44
14FU <fauzia53.u@gmail.com>
2022-09-27 10:28:59 +0100

    javascript intro

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict'
+
+console.log("Thanks Thom");
+
+console.log("First Name");
+console.log("Second Name");
+console.log("Where are you from");
+console.log("And your star sign");
+
+let make= "Mercedes Benz"
+let model= "C class"
+console.log("my favourite car is ",make,"and the model is",model);
